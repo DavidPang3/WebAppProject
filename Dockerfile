8f1b6d8e45bb1eb62ba53e0fef3c3d726d3d3f30
DavidPang3 <davidpang212121@gmail.com>
2024-09-26 15:04:29 -0400

    updated request.py i think its almost done?

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.8
+RUN apt-get update
+
+ENV HOME /root
+WORKDIR /root
+COPY . .
+# Download dependancies
+RUN pip3 install -r requirements.txt
+EXPOSE 8000
+CMD python3 -u server.py
--- util/request.py
+++ util/request.py
@@ -10,6 +10,31 @@ class Request:
         self.headers = {}
         self.cookies = {}
 
+        separatefirst = request.split(b'\r\n\r\n')
+
+        requesttransform = separatefirst[0].decode('utf-8')
+
+        lines = requesttransform.splitlines()
+                                        #GET / HTTP/1.1,    Host: localhost:8080,    Connection: keep-alive,    ,  
+
+        for eachline in lines:
+            if eachline:
+                if ':' in eachline:
+                    if 'Cookies' in eachline:
+                        spliteachline = eachline.split(': ', 1)
+                        self.headers[spliteachline[0]] = spliteachline[1]
+                    else:
+                        splitheaderline = eachline.split(': ', 1)
+                        self.headers[splitheaderline[0]] = splitheaderline[1]
+
+                else:
+                    self.method, self.path, self.http_version = eachline.split()
+
+        if separatefirst[1]:
+            self.body = separatefirst[1]
+
+            
+
 
 def test1():
     request = Request(b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\n\r\n')
@@ -17,18 +42,29 @@ def test1():
     assert "Host" in request.headers
     assert request.headers["Host"] == "localhost:8080"  # note: The leading space in the header value must be removed
     assert request.body == b""  # There is no body for this request.
+    #print(f"amount of bytes in body = {request.body}")
     # When parsing POST requests, the body must be in bytes, not str
-
     # This is the start of a simple way (ie. no external libraries) to test your code.
     # It's recommended that you complete this test and add others, including at least one
     # test using a POST request. Also, ensure that the types of all values are correct
 
+def test2():
+    request = Request(b'POST /path1231 HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nCookies: thisdobeacookie; thisanothercookie; thisathirdcookie\r\n\r\nHELLO WORLD!!!')
+    assert request.method == 'POST'
+    assert request.path == '/path1231'
+    assert request.http_version == 'HTTP/1.1'
+    assert request.body == b"HELLO WORLD!!!"
+    #print(f"cookie:header = {request.cookies}")
+    #print(f"method = {request.method}\n")
+    #print(f"path = {request.path}\n")
+    #print(f"http_version = {request.http_version}\n")
+    #print(f"amount of bytes in body = {request.body}")
+    print(f"headers = {request.headers}")
 
 if __name__ == '__main__':
-    test1()
-
-
-
+    test2()
+    #test1()
+    print("Tests Passed")
 
 
 
