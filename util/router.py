51548638a480f95031ea46016e508fd12a274fcc
DavidPang3 <davidpang212121@gmail.com>
2024-09-27 18:12:13 -0400

    just removed extra else ifs...

--- util/router.py
+++ util/router.py
@@ -3,6 +3,9 @@ class Router:
     def __init__(self):
         self.routes = []
 
+
+#method: GET, path: / , action: sendhtml, exact_path = false or true
+
     def add_route(self, method, path, action, exact_path=False):
         self.routes.append({'method':method, 'path':path, 'action':action, 'exact_path':exact_path})
 
@@ -11,20 +14,16 @@ class Router:
             if index['exact_path'] == True:
                 if index['method'] == request.method and index['path'] == request.path:
                     index['action'](request, handler)
-                    return
+         
                          
-            elif index['exact_path'] == False:
+            else:
                 if index['method']==request.method:
                     if request.path.startswith(index['path']):
                         index['action'](request, handler)
-                        return
                     if index['path'].startswith(request.path):
                         index['action'](request, handler)
-                        return
-                
-        else:
-            response = 'HTTP/1.1 404 Not Found\r\n Content-Type: text/plain\r\n Content-Length: 36\r\n\r\nThe requested content does not exist'
-            handler.request.sendall(response.encode())
-            return
-    
 
+                
+        response = 'HTTP/1.1 404 Not Found\r\n Content-Type: text/plain\r\n Content-Length: 36\r\n\r\nThe requested content does not exist'
+        handler.request.sendall(response.encode())
+    
