92ee23b194ded06e57cff3e6f5b1e64a79651a47
DavidPang3 <davidpang212121@gmail.com>
2024-09-27 14:52:37 -0400

    wtf?

--- util/router.py
+++ util/router.py
@@ -1,10 +1,23 @@
 class Router:
 
     def __init__(self):
-        pass
+        self.routes = []
 
     def add_route(self, method, path, action, exact_path=False):
-        pass
+        self.method = method
+        self.path = path
+        self.boolean = exact_path
+        self.function = action
+
+        
 
     def route_request(self, request, handler):
         pass
+
+def test1():
+    router = Router
+    print(f"routes: {router.routes}")
+
+
+if __name__ == '__main__':
+    test1()
