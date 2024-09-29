dc99c2e20fd02e15b4417b417245d6b7f0624b3f
DavidPang3 <davidpang212121@gmail.com>
2024-09-28 23:23:08 -0400

    fixed send_home method emojis added

--- server.py
+++ server.py
@@ -4,15 +4,37 @@ from util.router import Router
 from util.hello_path import hello_path
 
 #request to response
+
 def send_home(request, handler):
-        response = "HTTP/1.1 200 OK\r\nContent-Length: 10\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nhellomydud"
-        handler.request.sendall(response.encode())
+    file_path = open('public/index.html', 'r')
+    html_content = file_path.read()
+    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: {len(html_content.encode('utf-8'))}\r\nConnection: close\r\n\r\n{html_content}"
+    handler.request.sendall(response.encode('utf-8'))
+    
+def send_css(request, handler):
+    file_path = open('public/style.css', 'r')
+    css_content = file_path.read()
+    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=utf-8\r\nContent-Length: {len(css_content)}\r\nConnection: close\r\n\r\n{css_content}"
+    handler.request.sendall(response.encode('utf-8'))
+
+def send_javascript(request, handler):
+    javascript = ""
+    handler.request.sendall(javascript.encode())
+
+def send_images(request, handler):
+    images = ""
+    handler.request.sendall(images.encode())
+
+
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def __init__(self, request, client_address, server):
         self.router = Router()
         self.router.add_route("GET", "/", send_home, True)
+        #self.router.add_route("GET", "styles.css", send_css, True)
+        #self.router.add_route("GET", "/", send_javascript, True)
+        #self.router.add_route("GET", "/", send_images, True)
         super().__init__(request, client_address, server)
 
     def handle(self):
